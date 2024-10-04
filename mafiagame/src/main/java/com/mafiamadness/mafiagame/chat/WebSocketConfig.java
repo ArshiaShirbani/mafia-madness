01822a60ec84b2a11047adda87827ef3bc55d269
arshia <arshiashirbani1@gmail.com>
2024-10-03 18:12:50 -0700

    mafiagame directory to implement chat functionality

---
+++ mafiagame/src/main/java/com/mafiamadness/mafiagame/chat/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.mafiamadness.mafiagame.chat;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatWebSocketHandler chatWebSocketHandler;
+
+    public WebSocketConfig(ChatWebSocketHandler chatWebSocketHandler) {
+        this.chatWebSocketHandler = chatWebSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatWebSocketHandler, "/chatroom")
+                .setAllowedOrigins("*") // Allow all origins
+                .addInterceptors(new HttpSessionHandshakeInterceptor());
+    }
+}
---
+++ mafiagame/src/test/java/com/mafiamadness/mafiagame/MafiagameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mafiamadness.mafiagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafiagameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
